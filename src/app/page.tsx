87e099f78a19ad3105d25349db25528e4055b3cd
iamchiwon <iamchiwon@gmail.com>
2025-01-05 17:49:44 +0900

    todo list component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,45 +1,16 @@
 "use client";
 
-import Database from "@/libs/database";
-import { ToDoItem } from "@/shared/types/ToDoItem";
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import { ToDoList } from "./ToDoList";
 
 export default function Home() {
   const [greeting, setGreeting] = useState("");
-  const [todo, setTodo] = useState("");
-  const [todoList, setTodoList] = useState<ToDoItem[]>([]);
 
   const handleGreeting = async () => {
     const response = await fetch("/api/hello");
     const data = await response.json();
     setGreeting(data.message);
   };
-
-  const handleAddTodo = async () => {
-    await Database.createTodo(todo);
-    setTodo("");
-    fetchTodoList();
-  };
-
-  const fetchTodoList = async () => {
-    const todos = await Database.getTodoList();
-    setTodoList(todos);
-  };
-
-  useEffect(() => {
-    fetchTodoList();
-  }, []);
-
-  const handleDeleteTodo = async (id: string) => {
-    await Database.deleteTodo(id);
-    fetchTodoList();
-  };
-
-  const handleToggleTodo = async (id: string) => {
-    await Database.toggleTodo(id);
-    fetchTodoList();
-  };
-
   return (
     <div className="p-4">
       <button
@@ -49,42 +20,7 @@ export default function Home() {
         Greeting
       </button>
       <div>{greeting}</div>
-      <div className="pt-4">
-        <div className="text-2xl font-bold">Todo List</div>
-        <div className="flex gap-2">
-          <input
-            type="text"
-            placeholder="Add new todo"
-            className="border-gray-100 border-2 rounded-md p-2"
-            value={todo}
-            onChange={(e) => setTodo(e.target.value)}
-          />
-          <button
-            className="bg-blue-500 text-white p-2 rounded-md"
-            onClick={handleAddTodo}
-          >
-            Add
-          </button>
-        </div>
-        <div className="mt-4">
-          {todoList.map((todo) => (
-            <div className="flex gap-2 items-center" key={todo.id}>
-              <input
-                type="checkbox"
-                checked={todo.done}
-                onChange={() => handleToggleTodo(todo.id)}
-              />
-              {todo.content}
-              <button
-                className="text-red-500 ml-4 font-bold"
-                onClick={() => handleDeleteTodo(todo.id)}
-              >
-                DEL
-              </button>
-            </div>
-          ))}
-        </div>
-      </div>
+      <ToDoList />
     </div>
   );
 }
