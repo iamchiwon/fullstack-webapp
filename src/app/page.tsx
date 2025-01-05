92965a090f698d06fe32b3dc3891955696248b61
iamchiwon <iamchiwon@gmail.com>
2025-01-05 17:23:16 +0900

    todo list and create

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,34 @@
 "use client";
 
-import { useState } from "react";
+import Database from "@/libs/database";
+import { ToDoItem } from "@/shared/types/ToDoItem";
+import { useEffect, useState } from "react";
 
 export default function Home() {
   const [greeting, setGreeting] = useState("");
+  const [todo, setTodo] = useState("");
+  const [todoList, setTodoList] = useState<ToDoItem[]>([]);
 
   const handleGreeting = async () => {
     const response = await fetch("/api/hello");
     const data = await response.json();
     setGreeting(data.message);
   };
+
+  const handleAddTodo = async () => {
+    await Database.createTodo(todo);
+    setTodo("");
+    fetchTodoList();
+  };
+
+  const fetchTodoList = async () => {
+    const todos = await Database.getTodoList();
+    setTodoList(todos);
+  };
+
+  useEffect(() => {
+    fetchTodoList();
+  }, []);
 
   return (
     <div className="p-4">
@@ -20,6 +39,33 @@ export default function Home() {
         Greeting
       </button>
       <div>{greeting}</div>
+      <div className="pt-4">
+        <div className="text-2xl font-bold">Todo List</div>
+        <div className="flex gap-2">
+          <input
+            type="text"
+            placeholder="Add new todo"
+            className="border-gray-100 border-2 rounded-md p-2"
+            value={todo}
+            onChange={(e) => setTodo(e.target.value)}
+          />
+          <button
+            className="bg-blue-500 text-white p-2 rounded-md"
+            onClick={handleAddTodo}
+          >
+            Add
+          </button>
+        </div>
+        <div className="mt-4">
+          {todoList.map((todo) => (
+            <div className="flex gap-2 items-center" key={todo.id}>
+              <input type="checkbox" checked={todo.done} readOnly />
+              {todo.content}
+              <button className="text-red-500 ml-4 font-bold">DEL</button>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
